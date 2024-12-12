8a8599aa6895f4a0e4a5ea0619dc3bd796f0140b
Mahel Napo <MAHEL.NAPO92@login.cuny.edu>
2024-12-12 08:54:39 -0500

    more changes to get things deployed

--- flavorflow/server.js
+++ flavorflow/server.js
@@ -274,13 +274,17 @@ app.get('/api/restaurant/details', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  app.get('/healthz', (req, res) => {
+    res.status(200).send('OK');
   });
-}
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+  }
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
